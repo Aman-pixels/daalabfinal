56fa33ed9e7ef5cd0ddcc850b621f3b09b598b08
Aman-pixels <amankundal540@gmail.com>
2026-06-09 21:05:03 +0530

    tspchange

--- tsp1.java
+++ tsp1.java
@@ -5,6 +5,9 @@ public class tsp1 {
     static int N;
     static int minCost = Integer.MAX_VALUE;
 
+    static int[] bestPath;
+    static int[] currentPath;
+
     static void tsp(int[][] graph,
                     boolean[] visited,
                     int currPos,
@@ -14,26 +17,33 @@ public class tsp1 {
 
         if (count == N && graph[currPos][start] > 0) {
 
-            minCost = Math.min(
-                    minCost,
-                    cost + graph[currPos][start]);
+            int totalCost = cost + graph[currPos][start];
+
+            if (totalCost < minCost) {
+
+                minCost = totalCost;
+
+                for (int i = 0; i < N; i++) {
+                    bestPath[i] = currentPath[i];
+                }
+            }
 
             return;
         }
 
         for (int i = 0; i < N; i++) {
 
-            if (!visited[i] &&
-                graph[currPos][i] > 0) {
+            if (!visited[i] && graph[currPos][i] > 0) {
 
                 visited[i] = true;
+                currentPath[count] = i;
 
                 tsp(graph,
-                    visited,
-                    i,
-                    count + 1,
-                    cost + graph[currPos][i],
-                    start);
+                        visited,
+                        i,
+                        count + 1,
+                        cost + graph[currPos][i],
+                        start);
 
                 visited[i] = false;
             }
@@ -59,16 +69,23 @@ public class tsp1 {
 
         boolean[] visited = new boolean[N];
 
+        bestPath = new int[N];
+        currentPath = new int[N];
+
         visited[0] = true;
+        currentPath[0] = 0;
 
-        tsp(graph,
-            visited,
-            0,
-            1,
-            0,
-            0);
+        tsp(graph, visited, 0, 1, 0, 0);
 
-        System.out.println("Minimum Cost = " + minCost);
+        System.out.println("\nMinimum Cost = " + minCost);
+
+        System.out.print("Optimal Tour = ");
+
+        for (int i = 0; i < N; i++) {
+            System.out.print(bestPath[i] + " -> ");
+        }
+
+        System.out.println(bestPath[0]);
 
         sc.close();
     }
